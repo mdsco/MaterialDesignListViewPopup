3dd48f56811e284a59246561594d6075dcb08f20
mike <scoboria@gmail.com>
2017-05-01 20:49:58 -0400

    CardView with animations happening!

---
+++ app/src/main/java/com/example/mike/materialdesignlistviewpopup/CardViewRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.example.mike.materialdesignlistviewpopup;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.transition.AutoTransition;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+
+public class CardViewRecyclerViewAdapter extends RecyclerView.Adapter<CardViewRecyclerViewAdapter.CardViewHolder> {
+
+    private final List<String> list;
+    private final Context context;
+
+    public CardViewRecyclerViewAdapter(Context context, List<String> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public CardViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_card_item, parent, false);
+
+        CardViewHolder cardViewHolder = new CardViewHolder(listItem);
+        final AutoTransition transition = new AutoTransition();
+        transition.setDuration(100);
+
+        cardViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+
+            boolean visible = false;
+
+            @Override
+            public void onClick(View view) {
+                TextView hiddenText = (TextView) view.findViewById(R.id.hiddenText);
+
+                visible = !visible;
+                float elevation = visible ? 10.0f : 5.0f;
+                float zVal = visible ? 20.0f : 5.0f;
+
+                TransitionManager.beginDelayedTransition(parent, transition);
+                hiddenText.setVisibility(visible ? View.VISIBLE : View.GONE);
+                view.setElevation(elevation);
+                view.setTranslationZ(zVal);
+            }
+
+        });
+
+        return cardViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CardViewHolder holder, int position) {
+        holder.text.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView text;
+
+        public CardViewHolder(View itemView) {
+            super(itemView);
+//            itemView.setOnClickListener(this);
+            text = (TextView) itemView.findViewById(R.id.textView);
+        }
+
+        @Override
+        public void onClick(View view) {
+//            Log.v("yeah", view.toString());
+//            Log.v("What", "Da fuq");
+//            CardView cardView = (CardView) view;
+//            cardView.setElevation(40.0f);
+//            cardView.setZ(40.0f);
+//            TextView hidden = (TextView) view.findViewById(R.id.hiddenText);
+
+//            TransitionManager.beginDelayedTransition(parent);
+//            hidden.setVisibility(true);
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/mike/materialdesignlistviewpopup/MainActivity.java
+++ app/src/main/java/com/example/mike/materialdesignlistviewpopup/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mike.materialdesignlistviewpopup;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> names;
     ArrayAdapter<String> adapter;
+    RecyclerView.Adapter<CardViewRecyclerViewAdapter.CardViewHolder> cardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,25 @@ public class MainActivity extends AppCompatActivity {
         names.add("Joe");
         names.add("Fronk");
         names.add("Mary");
+        names.add("Jules");
+        names.add("Shaina");
+        names.add("Andy");
+        names.add("Joanna");
 
-        adapter = new PopUpArrayAdapter(this, R.id.activity_main, R.id.pop_up_list_view, names);
+        CardViewRecyclerViewAdapter adapter = new CardViewRecyclerViewAdapter(getApplicationContext(), names);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
 
-        ListView listView = (ListView) findViewById(R.id.pop_up_list_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
-        listView.setAdapter(adapter);
+//        adapter = new PopUpArrayAdapter(this, R.id.activity_main, R.id.pop_up_list_view, names);
+//
+//        ListView listView = (ListView) findViewById(R.id.pop_up_list_view);
+//
+//        listView.setAdapter(adapter);
 
     }
 }
